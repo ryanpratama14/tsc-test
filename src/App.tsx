47bf75579924644317fd21132d909d9c04aac11c
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-05-04 01:59:47 +0300

    adjusted

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import { Icon } from "@iconify/react";
 import { Toaster, toast } from "react-hot-toast";
